4cc20ed3d54e42ef8f56a23d8ba1559bbc9a8462
acsauk <acsauk@gmail.com>
2016-07-08 12:19:26 +0100

    SRP challengen 15 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,26 +1,32 @@
+require_relative 'bike'
 
 class DockingStation
 
 def initialize
-
-@capacity = []
-
+  @capacity = []
 end
 
 
 def release_bike
-
-
-  fail 'No bikes available' unless @capacity.length > 0
-
+  fail 'No bikes available' if empty?
   @capacity.pop
-
-
 end
 
+
 def dock(bike)
-  raise "Station full - come back later" if @capacity.length == 20
+  raise "Station full - come back later" if full?
   @capacity << bike
-  end
+end
+
+
+private
+
+def empty?
+  @capacity.empty?
+end
+
+def full?
+  @capacity.count >= 20
+end
 
 end
